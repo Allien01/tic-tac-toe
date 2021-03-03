569ddbf385fd950d7a8ddb1a035db4bfd4547e2a
Aline Galdino <alinegal@gmail.com>
2021-03-02 23:13:00 -0300

    display the grid

--- src/tic-tac-toe.py
+++ src/tic-tac-toe.py
@@ -1,3 +1,10 @@
-grid = ['X O X', 'O X O', 'X X O']
-for line in grid:
-    print(line)
+# grid = ['X O X', 'O X O', 'X X O']
+# for line in grid:
+#    print(line)
+
+entrada = input("Enter cells")
+print("---------")
+print("| " + entrada[0] + " " + entrada[1] + " " + entrada[2] + " |")
+print("| " + entrada[3] + " " + entrada[4] + " " + entrada[5] + " |")
+print("| " + entrada[6] + " " + entrada[7] + " " + entrada[8] + " |")
+print("---------")
